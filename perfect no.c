3fdda465018b39def50dff3d738d003960adb9cb
Mohneesh-Sharma <vandnasharma031985@gmail.com>
2026-04-12 17:27:56 +0530

    To check number is perfect or not

---
+++ perfect no.c
@@ -0,0 +1,17 @@
+//TO CHECH FOR NUMBER IS PERFECT OR NOT
+#include <stdio.h>
+
+int main() {
+    int i,f=0,n;
+    printf("Enter your number: ");
+    scanf("%d",&n);
+    for(i=1;i<=n/2;i++){
+        if(n%i==0)
+        f+=i;
+    }
+    if(f==n)
+    printf("Number is perfect");
+    else
+    printf("Number is not perfect");
+    return 0;
+}
