211f24e1d73a1faa6248f0c877521c23bf409285
Mohneesh-Sharma <vandnasharma031985@gmail.com>
2026-04-13 07:08:43 +0530

    To input n integers and find min , max using pointers

---
+++ minmax pointers.c
@@ -0,0 +1,27 @@
+{
+    int *ptr,n,max,min,i;
+    printf("enter the number of integers to be entered  : ");
+    scanf("%d",&n);
+    ptr=(int*)malloc(n*sizeof(int));
+    if(ptr==NULL){
+    printf("*************not enough space*************");
+    exit(0);
+    }
+    else{
+        printf("enter the integers :\n");
+        for(i=0;i<n;i++){
+            scanf("%d",ptr+i);
+        }
+        max=*ptr;
+        min=*ptr;
+        for(i=1;i<n;i++){
+            if(max<*(ptr+i))
+            max=*(ptr+i);
+            if(min>*(ptr+i))
+            min=*(ptr+i);
+        }
+        printf("The MAXIMUM value = %d\nThe MINIMUN value = %d",max,min);
+    }
+    free(ptr);
+    return 0;
+}
